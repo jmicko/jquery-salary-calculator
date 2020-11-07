0cb830e8c0b50633a680c1564f136492c56c2063
John Micko <jrmicko5012@gmail.com>
2020-11-07 13:54:35 -0600

    monthly total updates when new emplyoee added

--- client.js
+++ client.js
@@ -1,7 +1,39 @@
 console.log('ready');
 
 $(document).ready(readyNow);
+let totalMonthly = 0
 
 function readyNow() {
     console.log('jQuery connected');
+    $('#submit-button').on('click', submitEmployee);
+    $('main').append(`<h2 id='monthly-total'>Total Monthly: $${totalMonthly}</h2>`)
+}
+
+
+
+function submitEmployee(event) {
+    event.preventDefault();
+    let employee = {
+        firstName: $('#in-first-name').val(),
+        lastName: $('#in-last-name').val(),
+        id: $('#in-id').val(),
+        title: $('#in-title').val(),
+        salary: $('#in-annual-salary').val(),
+    };
+    
+    $('.employee-list').append(`
+    <tr>
+    <td>${employee.firstName}</td>
+    <td>${employee.lastName}</td>
+    <td>${employee.id}</td>
+    <td>${employee.title}</td>
+    <td>$${employee.salary}</td>
+    <td><button>Delete</button></td>
+    </tr>
+    `);
+    // change the salary string to a number so we can just add it to the total
+    let salaryNum = Number(employee.salary.replace(',', ''));
+    totalMonthly += salaryNum;
+    $('#monthly-total').text(`Total Monthly: $${totalMonthly}`)
+    console.log(salaryNum, employee.salary);
 }
