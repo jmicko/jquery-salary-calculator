f9065632e01fc39fe30d35c09fc9934144915fd8
John Micko <jrmicko5012@gmail.com>
2020-11-07 21:51:37 -0600

    employee can only be added when all fields filled

--- client.js
+++ client.js
@@ -3,18 +3,20 @@ console.log('ready');
 $(document).ready(readyNow);
 let totalMonthly = 0
 
+
 function readyNow() {
     console.log('jQuery connected');
     $('.submit-button').on('click', submitEmployee);
-    $('main').append(`<h2 id='monthly-total'>Total Monthly: $${totalMonthly}</h2>`)
-
+    $('main').append(`<h2 id='monthly-total'>Total Monthly: $${totalMonthly}</h2>`);
+    $('.employee-list').on('click', '.delete-cell', deleteEmployee);
 }
 
 
-
+// take input from text boxes and add them to the table in the DOM
 function submitEmployee(event) {
     event.preventDefault();
 
+    // grab values from all input boxes and store them in an employee object
     let employee = {
         firstName: $('#in-first-name').val(),
         lastName: $('#in-last-name').val(),
@@ -23,7 +25,20 @@ function submitEmployee(event) {
         salary: $('#in-annual-salary').val(),
     };
 
-    if (employee.firstName) {
+    // find out if all input fields have data in them
+    function fieldsFull() {
+        // loop through employee object
+        for (const val in employee) {
+            console.log(`value of the key is:`, employee[val]);
+            if (!employee[val]) {
+                console.log(`it's false`, employee[val]);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    if (fieldsFull()) {
         console.log('there is an employee', employee);
         $('.employee-list').append(
         `<tr>
@@ -35,11 +50,11 @@ function submitEmployee(event) {
             <td class='delete-cell'><button class='delete-button'>Terminate</button></td>
         </tr>`
         );
+        // change the salary string to a number so we can just add it to the total
+        let salaryNum = Number(employee.salary.replace(',', ''));
+        totalMonthly += salaryNum;
+        $('#monthly-total').text(`Total Monthly: $${totalMonthly}`);
+        console.log(salaryNum, employee.salary);
     }
-    
-    // change the salary string to a number so we can just add it to the total
-    let salaryNum = Number(employee.salary.replace(',', ''));
-    totalMonthly += salaryNum;
-    $('#monthly-total').text(`Total Monthly: $${totalMonthly}`);
-    console.log(salaryNum, employee.salary);
 }
+
